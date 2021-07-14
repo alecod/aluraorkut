124b151c3a2a5108bf20c2073a1c405b61ed02d9
Ale Junqueira <alessandro.junqueira@intergalaxy.io>
2021-07-14 13:06:42 -0300

    up

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const Box = styled.div`
+    background: #fff;
+    border-radius: 8px;
+
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <main>
+      <Box>
+        imagem
+      </Box>
+      <Box>
+        bem vindo
+      </Box>
+      <Box>
+      comunidades
+      </Box>
+    </main>
+  )
 }
